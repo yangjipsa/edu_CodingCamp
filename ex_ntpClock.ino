6cfbcf38653382784b3a3ee05479e27b1ed925ac
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2024-07-26 14:42:52 +0900

    Create ex_ntpClock.ino

---
+++ ex_ntpClock.ino
@@ -0,0 +1,47 @@
+// board : wemos d1 r1
+
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+// #include <WiFiUdp.h>
+
+#define STASSID "input your wifi SSID" //와이파이 검색했을때 뜨는 이름
+#define STAPSK  "input your wifi PASSWORD" //패스워드
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+const long utcOffsetInSeconds = 32400; // every UTC +1.00, +3600
+String Week[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+void setup() 
+{
+  Serial.begin(9600);
+  
+  // WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay (500);
+    Serial.print(".");
+  }
+  Serial.print("WiFi connected");
+  Serial.print("IP address : ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize NTP  
+  timeClient.begin();
+}
+
+void loop() {
+  timeClient.update();
+  
+  Serial.print(Week[timeClient.getDay()]);
+  Serial.print(" ");
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
+  delay(1000);
+}
