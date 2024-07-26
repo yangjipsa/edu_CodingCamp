f251912639e9e676aacc82cc032f91c6183833cf
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2024-07-26 14:16:02 +0900

    Create ex_sw_toggle1.ino

---
+++ ex_sw_toggle1.ino
@@ -0,0 +1,28 @@
+//board : wemos d1 r1
+#define pinSW1  D2
+#define pinLED1 D4
+
+bool cState1 = false; // current state of sw1
+bool lState2 = false; // last state of sw2
+
+bool valLED1 = false;
+
+void setup()
+{
+  pinMode(pinSW1, INPUT);
+  pinMode(pinLED1, OUTPUT);
+}
+
+void loop() 
+{
+  cState1 = digitalRead(pinSW1); // read SW1 value
+  delay(10); // for debouncing
+  
+  if (cState1 < lState1) // detecting rising edge
+  {
+    valLED1 = !valLED1;
+    digitalWrite(pinLED1, valLED1);
+  }
+
+  lState1 = cState1; // sw1 value update
+}
