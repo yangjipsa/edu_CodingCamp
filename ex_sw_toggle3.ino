a2d22cc8bfcfac0b9d588ebc54eac2dc21dcc4e9
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2024-07-26 14:20:06 +0900

    Create ex_sw_toggle3.ino

---
+++ ex_sw_toggle3.ino
@@ -0,0 +1,24 @@
+//board : wemos d1 r1
+#define pinSW1  D2
+#define pinLED1 D4
+
+bool valLED1 = false;
+
+void setup()
+{
+  pinMode(pinSW1, INPUT);
+  pinMode(pinLED1, OUTPUT);
+}
+
+void loop() 
+{
+  bool valSW1 = digitalRead(pinSW1); // read SW1 value
+
+  if(valSW1)
+  {
+    while(!digitalRead(pinSW1)); //waiting switch off
+    
+    valLED1 = !valLED1;
+    digitalWrite(pinLED1, valLED1);
+  }
+}
